ec00fcf57fdaf2016a49669f6ffd3cc9c6b7cfca
Anuragchindaliya <ak1148383@gmail.com>
2021-11-05 19:59:34 +0530

    Add: country code and extract msg

--- script.js
+++ script.js
@@ -1,71 +1,109 @@
+
 window.onload = function () {
   // alert("loaded");
   // confirm("are you hacker");
   var input = document.querySelector("#link");
   var go = document.querySelector("#go");
   var errorMsg = document.querySelector("#errorMsg");
+  var dialCodeInput = document.querySelector("#dial-code");
+  var textMsg = document.querySelector("#textMsg");
+  var dialCodeListValue = document.querySelector("#countryCode");
+
   input.focus();
+
+  input.addEventListener("input",checkLength);
   function checkLength(e) {
     if (e.target.value.length == 10) {
       input.style.border = "2px solid rgb(26, 196, 91)";
+    }else{
+      input.style.border = "2px solid rgb(209, 0, 0)";
     }
   }
+  
   //   input.addEventListener("change", ()=>{checkLength});
 
   function clipboardText() {
     navigator.clipboard
       .readText()
       .then((text) => {
+        //remove space
         var number = text.replace(/\s/g, "");
+        var countryCode = "+91";
+        
 
+        
         if (text[0] === "+") {
           //extract country code
-          var countryCodeLength = number.length - 10;
-          var countryCode = number.substr(0, countryCodeLength);
-          number = number.substr(countryCodeLength, 10);
+          countryCode = number.substr(0, number.length-10);
+
+          //extract mobile number
+          number = number.substr(countryCode.length, 10);
+        }else if(!isNaN(number) && number.length >10){
+          
+         countryCode = `+${number.substr(0,number.length-10)}`;
+         console.log(countryCode)
         }
-        console.log(countryCode, number);
+        // console.log(countryCode, number);
         if (isNaN(number)) {
           if (
             confirm(
-              `Please copy only number \nDo you want to go with this:\n${text}`
+              `Please copy only number \nDo you want to insert it into message :\n${text}`
             )
           ) {
-            input.value = text;
+            // input.value = text;
+            textMsg.value = number;
+            input.focus();
           }
         } else {
-          input.value = number;
+          dialCodeInput.value = countryCode;
+          dialCodeListValue.value = countryCode;
+          input.value = number.substring(number.length-10);
           if (input.value.length == 10) {
             input.style.border = "2px solid rgb(26, 196, 91)";
           }
         }
       })
       .catch((err) => {
-        input.value = "9811457143";
+        input.value = `${err}`;
         errorMsg.innerHTML = `error in clip board`;
       });
   }
-  clipboardText();
+  if (navigator.clipboard) {
+    clipboardText();
+  } else {
+    errorMsg = "Clipboard access is not supportable in your device";
+  }
+
   window.addEventListener("keyup", function (event) {
     // Number 13 is the "Enter" key on the keyboard
     if (event.keyCode === 13) {
       // Cancel the default action, if needed
       event.preventDefault();
       // Trigger the button element with a click
-      m4wLink(input.value);
+      if(dialCodeListValue.value){
+        console.log("dial Code value"+dialCodeListValue.value);
+      }else{
+        console.log(dialCodeListValue);
+      }
+      
+      m4wLink(input.value,textMsg.value,dialCodeListValue.value);
     }
   });
   go.addEventListener("click", () => {
-    m4wLink(input.value);
+    m4wLink(input.value,textMsg.value,dialCodeListValue.value);
   });
-  function m4wLink(text) {
+  function m4wLink(text,msg,dialCode) {
+    // console.log(dialCode);
     if (text.length == 10) {
-      window.location.replace(`https://wa.me/91${text}`);
+      // window.location.replace(`https://wa.me/91${text}`);
+      
+      window.open(`https://wa.me/${dialCode}${text}?text=${msg}`, "_blank");
     } else {
       if (
         confirm("number is not in correct format do you want to go with this")
       ) {
-        window.location.replace(`https://wa.me/91${text}`);
+        // window.location.replace(`https://wa.me/91${text}`);
+        window.open(`https://wa.me/${dialCode}${text}?text${msg}`, "_blank");
       }
     }
   }
